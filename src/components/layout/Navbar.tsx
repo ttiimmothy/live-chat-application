7ef01df74b38a15ef97d9f66f309d0c5c89840b6
ttiimmothy <dinosauli2006@gmail.com>
2023-10-30 21:31:46 -0400

    Feature: use Link instead of a tag

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { googleSignIn, signOut } from "../../utils/auth";
 import { auth } from "../../../firebase.config";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   const [user] = useAuthState(auth);
@@ -8,9 +9,9 @@ const Navbar: React.FC = () => {
   return (
     <nav className="flex justify-center border-b-2">
       <div className="w-full h-12 flex justify-between items-center max-w-5xl mx-auto px-8">
-        <a className="text-xl font-semibold text-gray-600" href="/">
+        <Link className="text-xl font-semibold text-gray-600" href="/">
           Chat rooms
-        </a>
+        </Link>
         {user ? (
           <button onClick={signOut} className="sign-out" type="button">
             Sign Out
